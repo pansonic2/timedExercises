1cf9678858b525111732a240db75e9e4d96750ac
StanislavAlexandrov <stanislavalexandrov@gmail.com>
2019-02-13 13:12:48 +0500

    timedExercise1

---
+++ js/index.js
@@ -0,0 +1,20 @@
+listQ1 = ["Here you go...", "This is number 2.", "This is number 333", "This is the next one"];
+console.log(list[0]);
+function timedText() {
+  i = 0;
+  setTimeout(myTimeout1, 20)
+  }
+  function myTimeout1() {
+    if (listQ[i]){ 
+    document.getElementById("demo").innerHTML = listQ[i];
+    i += 1;
+    setTimeout(myTimeout1, 2000);
+    }
+    else {
+      document.getElementById("demo").innerHTML = "End of List."  
+    }
+  }
+  
+
+
+
---
+++ toBe/js/index.js
@@ -0,0 +1,20 @@
+listQ = ["1. Who ... you?", "2. What ... your name?", "3. Where ... you from?", "4. How old ... you?", "5. I ... Maria.", "6. I ... 20.", "7. I ... a consultant.", "8. They ... at work.", "9. \n I \n ... \n f \n i \n n \n e \n ."];
+console.log(list[0]);
+function timedText() {
+  i = 0;
+  setTimeout(myTimeout1, 20);
+  }
+  function myTimeout1() {
+    if (listQ[i]){ 
+    document.getElementById("demo").innerHTML = listQ[i];
+    i += 1;
+    setTimeout(myTimeout1, 1000);
+    }
+    else {
+      document.getElementById("demo").innerHTML = "End of List.";
+    }
+  }
+  
+
+
+
